59a59e2ace4f27b17c3f4cf6a330cf6829f87a1b
Bloodmallet <kruse.peter.1990@gmail.com>
2018-08-09 22:39:01 +0200

    how about not breaking the pip install?

--- setup.py
+++ setup.py
@@ -2,21 +2,23 @@
 
 from setuptools import setup
 
+
 def readme():
-  with open('README.txt') as f:
-    return f.read()
+    with open('README.md') as f:
+        return f.read()
+
 
 setup(name='simc_support',
-  version='1.0',
-  author='Bloodmallet(EU)',
-  author_email='kruse.peter.1990@gmail.com',
-  description='This package offers some World of Warcraft related ingame data regularly used for simulations and some basic input test for standard values of SimulationCraft.',
-  long_description=readme(),
-  url='https://github.com/Bloodmallet/simc_support',
-  packages=['simc_support'],
-  package_data={
-    "": ["*.txt", ],
-  },
-  python_requires='>3.5',
-  license='GNU GENERAL PUBLIC LICENSE',
-)
+      version='8.0.1.0',
+      author='Bloodmallet(EU)',
+      author_email='kruse.peter.1990@gmail.com',
+      description='This package offers some World of Warcraft related ingame data regularly used for simulations and some basic input test for standard values of SimulationCraft.',
+      long_description=readme(),
+      url='https://github.com/Bloodmallet/simc_support',
+      packages=['simc_support'],
+      package_data={
+          "": ["*.md", ],
+      },
+      python_requires='>=3.6',
+      license='GNU GENERAL PUBLIC LICENSE',
+      )
