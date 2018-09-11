2072aaf5c25bef21b9b96626da203f0ad7cb522e
Bloodmallet <kruse.peter.1990@gmail.com>
2018-09-11 15:35:04 +0200

    add Dread Aspirant's Medallion trinket

--- simc_support/wow_lib.py
+++ simc_support/wow_lib.py
@@ -848,6 +848,10 @@ __trinket_list = [
         name="Dread Gladiator's Insignia", item_id="161676", min_itemlevel=280, max_itemlevel=TITANFORGE_CAP,
         max_itemlevel_drop=TRADER_TOKEN, agility=True, intellect=True, strength=True, melee=True, ranged=True
     ),
+    Trinket(  # pvp
+        name="Dread Aspirant's Medallion", item_id="162897", min_itemlevel=280, max_itemlevel=TITANFORGE_CAP,
+        max_itemlevel_drop=TRADER_TOKEN, agility=True, intellect=True, strength=True, melee=True, ranged=True
+    ),
 
     # Trinket(
     #     name="", item_id="", min_itemlevel=WORLD_QUEST_ITEMLEVEL, max_itemlevel=TITANFORGE_CAP,
