56935ff228d48ad2ecf119a1fd0d3e4ebd4fd383
Bloodmallet <kruse.peter.1990@gmail.com>
2019-01-17 19:00:18 +0100

    update default itemlevels

--- simc_support/wow_lib.py
+++ simc_support/wow_lib.py
@@ -8,11 +8,11 @@ import json
 # these values are used throughout the code to determine itemlevel "borders" of items
 # usually used as a catch up mechanic, drops usually all items from previous content
 TRADER_TOKEN = 300
-WORLD_QUEST_ITEMLEVEL = 370
+WORLD_QUEST_ITEMLEVEL = 385
 # standard dungeon itemlevel (normal, max level dungeon)
-DUNGEON_ITEMLEVEL = 310
+DUNGEON_ITEMLEVEL = 340
 # highest available itemlevel from m+ dungeons. weekly chest is NOT included here
-M_PLUS_ITEMLEVEL = 370
+M_PLUS_ITEMLEVEL = 400
 # currently highest itemlevel titanforging cap
 TITANFORGE_CAP = 425
 EXPANSION_START_ITEMLEVEL = 280
