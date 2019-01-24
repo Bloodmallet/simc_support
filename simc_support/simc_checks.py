052b498f7e074a8913a3057c3fa57822a06b3f98
Bloodmallet <kruse.peter.1990@gmail.com>
2019-01-24 15:52:46 +0100

    breaking changes: refactor simc_checks
    simc_checks offers now two classes.
    FightStyle and Tier
    Both are intended to provide simulationcraft relevant data.
    FightStyle in this case knows valid simulationcraft fight styles
    Use FightStyle.PATCHWERK for example to ensure the correct spelling of this fight style.
    Same logic applies to Tier.

--- simc_support/simc_checks.py
+++ simc_support/simc_checks.py
@@ -1,151 +1,113 @@
 # -*- coding: utf-8 -*-
-## Lib to check simc_options input values
+""" Lib to check simc_options input values
+"""
+from typing import Union
+
+class FightStyle(object):
+  """Collection of all fight style related information
+    """
+
+  PATCHWERK = "patchwerk"
+  LIGHTMOVEMENT = "lightmovement"
+  HEAVYMOVEMENT = "heavymovement"
+  HECTICADDCLEAVE = "hecticaddcleave"
+  BEASTLORD = "beastlord"
+  HELTERSKELTER = "helterskelter"
+  DUNGEONSLICE = "dungeonslice"
 
 
-##
-## @brief      Gets all fight styles.
-##
-## @return     The fight styles.
-##
-def get_fight_styles():
-  return (
-    "patchwerk",
-    "lightmovement",
-    "heavymovement",
-    "hecticaddcleave",
-    "beastlord",
-    "helterskelter"
-  )
+
+  def get_fight_styles(self) -> tuple:
+    """Returns a touple of all fight styles
+
+    Returns:
+      touple -- Containing all fight styles
+    """
+
+    return (
+      self.PATCHWERK,
+      self.LIGHTMOVEMENT,
+      self.HEAVYMOVEMENT,
+      self.HECTICADDCLEAVE,
+      self.BEASTLORD,
+      self.HELTERSKELTER,
+      self.DUNGEONSLICE
+    )
 
 
-##
-## @brief      Gets the profiles of the current addon.
-##
-## @return     The profiles.
-##
-def get_profiles():
-  return (
-    "PR"
-    "T19",
-    "T20",
-    "T21"
-  )
+  def is_fight_style(self, fight_style: Union[str,list]) -> bool:
+    """Returns true if fight_style is known and spelled correctly.
+
+    Arguments:
+      fight_style {str,list} -- [description]
+
+    Returns:
+      bool -- Known and spelled correctly fight style/-s
+    """
+
+    # downwards compatibility
+    if type(fight_style) == list:
+      return self.is_fight_style_list(fight_style)
+    fight_style_list = self.get_fight_styles()
+    if not fight_style in fight_style_list:
+      return False
+    return True
 
 
-##
-## @brief      Gets the tiers for the current addon
-##
-## @return     The tiers.
-##
-def get_tiers():
-  return (
-    "19",
-    "20",
-    "21"
-  )
+  def is_fight_style_list(self, fight_styles: list) -> bool:
+    """Returns true if all fight styles are spelled correctly and are known.
+
+    Arguments:
+      fight_styles {list} -- [description]
+
+    Returns:
+      bool -- [description]
+    """
+
+    fight_style_list = self.get_fight_styles()
+    for fight_style in fight_styles:
+      if not type(fight_style) == str:
+        return False
+      if not fight_style in fight_style_list:
+        return False
+    return True
 
 
-##
-## @brief      Validates the input fight style.
-##
-## @param      fight_style  The fight style like in SimC options
-##
-## @return     True if fight_style matches predetermined SimC-styles
-##
-def is_fight_style(fight_style):
-  # downwards compatibility
-  if type(fight_style) == list:
-    return is_fight_style_list(fight_style)
-  fight_style_list = get_fight_styles()
-  if not str(fight_style).lower() in fight_style_list:
+
+
+class Tier(object):
+  PR = "PR"
+  T21 = "T21"
+  T22 = "T22"
+  T23 = "T23"
+
+
+  def get_tiers(self) -> tuple:
+    """Return a tuple of all known tiers.
+
+    Returns:
+      tuple -- [description]
+    """
+
+    return (
+      self.PR,
+      self.T21,
+      self.T22,
+      self.T23
+    )
+
+
+  def is_tier(self, tier: str) -> bool:
+    """Returns true if tier is known.
+
+    Arguments:
+      profile {str} -- [description]
+
+    Returns:
+      bool -- [description]
+    """
+
+    for base_profile in self.get_tiers():
+      if tier == base_profile:
+        return True
     return False
-  return True
-
-
-##
-## @brief      Validates the input fight style.
-##
-## @param      fight_styles  The fight styles list like in SimC options
-##
-## @return     True if fight_style matches predetermined SimC-styles
-##
-def is_fight_style_list(fight_styles):
-  fight_style_list = get_fight_styles()
-  for fight_style in fight_styles:
-    if not type(fight_style) == str:
-      return False
-    if not str(fight_style).lower() in fight_style_list:
-      return False
-  return True
-
-##
-## @brief      Determines if iteration is a number as a string and greater than
-##             5000.
-##
-## @param      iterations  The iterations
-##
-## @return     True if iterations is string and greater than 5000, False
-##             otherwise.
-##
-def is_iteration(iterations):
-  if type(iterations) is str:
-    if int(iterations) >= 5000:
-      return True
-  return False
-
-
-##
-## @brief      Determines if profile is a valid input based on simc profiles.
-##
-## @param      profile  The profile
-##
-## @return     True if profile matches simc profiles, False otherwise.
-##
-def is_profile(profile):
-  for base_profile in get_profiles():
-    if profile.lower() == base_profile.lower():
-      return True
-  return False
-
-
-##
-## @brief      Determines if target error is string and < 0.5 and >= 0.0.
-##
-## @param      target_error  The target error
-##
-## @return     True if target error is string and < 0.5 and >= 0.0, False
-##             otherwise.
-##
-def is_target_error(target_error):
-  if type(target_error) is str:
-    if 0.5 >= float(target_error) >= 0.0:
-      return True
-  return False
-
-
-##
-## @brief      Determines if threads is useable for simc.
-##
-## @param      threads  The threads
-##
-## @return     True if threads, False otherwise.
-##
-def is_threads(threads):
-  if type(threads) == str:
-    if threads == "" or int(threads) > 0:
-      return True
-  return False
-
-
-##
-## @brief      Determines if tier number matches the current (addon) available tiers.
-##
-## @param      tier_number  The tier number
-##
-## @return     True if tier number, False otherwise.
-##
-def is_tier_number(tier_number):
-  if type(tier_number) == str:
-    if tier_number in get_tiers():
-      return True
-  return False
