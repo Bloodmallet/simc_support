5072e6bf2ee1a3f5c5e1f9db25b196617d266a67
Bloodmallet <kruse.peter.1990@gmail.com>
2019-06-24 20:26:35 +0200

    data update for 8.2 early release
    Missing: Trinkets, Azerite Traits,...

--- simc_support/wow_lib.py
+++ simc_support/wow_lib.py
@@ -1017,6 +1017,282 @@ __trinket_list = [
     # ),
 ]
 
+__essences = {
+    # "2": { # done tank
+    #     'name': "Azeroth's Undying Gift",
+    #     'major': {
+    #         'name': "Azeroth's Undying Gift",
+    #         'spell_id': 298081
+    #     },
+    #     'minor':{
+    #         'name': "Hardened Azerite",
+    #         'spell_id': 298083
+    #     },
+    #     'item_name': "Tempered Azerite Formation",
+    #     'item_id': 168537
+    # },
+    # "3": { # done tank
+    #     'name': "Suppressing Pulse",
+    #     'major': {
+    #         'name': "Suppressing Pulse",
+    #         'spell_id': 300010
+    #     },
+    #     'minor':{
+    #         'name': "Sphere of Suppression",
+    #         'spell_id': 300013
+    #     },
+    #     'item_name': "Sphere of Leeched Mobility",
+    #     'item_id': 168580
+    # },
+    "4": { # done
+        'name': "Worldvein Resonance",
+        'major': {
+            'name': "Worldvein Resonance",
+            'spell_id': 299334
+        },
+        'minor':{
+            'name': "Lifeblood",
+            'spell_id': 299333
+        },
+        'item_name': "Fluctuating Worldvein",
+        'item_id': 168617
+    },
+    "5": { # done
+        'name': "Essence of the Focusing Iris",
+        'major': {
+            'name': "Focused Azerite Beam",
+            'spell_id': 299338
+        },
+        'minor':{
+            'name': "Focused Energy",
+            'spell_id': 299337
+        },
+        'item_name': "Stabilizing Lens of the Focusing Iris",
+        'item_id': 168622
+    },
+    "6": { # done
+        'name': "Purification Protocol",
+        'major': {
+            'name': "Purifying Blast",
+            'spell_id': 299347
+        },
+        'minor':{
+            'name': "Purification Protocol",
+            'spell_id': 299346
+        },
+        'item_name': "Enhanced Purification Protocols",
+        'item_id': 168860
+    },
+    # "7": { # done tank
+    #     'name': "Anima of Death",
+    #     'major': {
+    #         'name': "Anima of Death",
+    #         'spell_id': 300003
+    #     },
+    #     'minor':{
+    #         'name': "Anima of Life",
+    #         'spell_id': 300005
+    #     },
+    #     'item_name': "Fetish of the Hidden Labyrinths",
+    #     'item_id': 168559
+    # },
+    "12": { # done
+        'name': "The Crucible of Flame",
+        'major': {
+            'name': "Concentrated Flame",
+            'spell_id': 299353
+        },
+        'minor':{
+            'name': "Ancient Flame",
+            'spell_id': 299350
+        },
+        'item_name': "Tempered Scale of the Scarlet Broodmother",
+        'item_id': 168613
+    },
+    # "13": { # done tank
+    #     'name': "Empowered Null Barrier",
+    #     'major': {
+    #         'name': "Empowered Null Barrier",
+    #         'spell_id': 300016
+    #     },
+    #     'minor':{
+    #         'name': "Null Barrier",
+    #         'spell_id': 300020
+    #     },
+    #     'item_name': "Null Force Nullifier",
+    #     'item_id': 168568
+    # },
+    "14": { # done
+        'name': "Condensed Life-Force",
+        'major': {
+            'name': "Guardian of Azeroth",
+            'spell_id': 299358
+        },
+        'minor':{
+            'name': "Condensed Life-Force",
+            'spell_id': 299357
+        },
+        'item_name': "Resonating Elemental Heart",
+        'item_id': 168856
+    },
+    "15": { # done
+        'name': "Ripple in Space",
+        'major': {
+            'name': "Ripple in Space",
+            'spell_id': 302983
+        },
+        'minor':{
+            'name': "Reality Shift",
+            'spell_id': 302985
+        },
+        'item_name': "Stalwart Battlefield Memento",
+        'item_id': 168852
+    },
+    # "17": { # done
+    #     'name': "The Ever-Rising Tide",
+    #     'major': {
+    #         'name': "Overcharge Mana",
+    #         'spell_id': 299876
+    #     },
+    #     'minor':{
+    #         'name': "The Ever-Rising Tide",
+    #         'spell_id': 299879
+    #     },
+    #     'item_name': "Tome of the Quickening Tides",
+    #     'item_id': 168930
+    # },
+    # "18": { # done
+    #     'name': "Artifice of Time",
+    #     'major': {
+    #         'name': "Standstill",
+    #         'spell_id': 299883
+    #     },
+    #     'minor':{
+    #         'name': "Artifice of Time",
+    #         'spell_id': 299887
+    #     },
+    #     'item_name': "Azerite-Fueled Timequartz",
+    #     'item_id': 168922
+    # },
+    # "19": { # done
+    #     'name': "The Well of Existence",
+    #     'major': {
+    #         'name': "Refreshment",
+    #         'spell_id': 299933
+    #     },
+    #     'minor':{
+    #         'name': "The Well of Existence",
+    #         'spell_id': 299936
+    #     },
+    #     'item_name': "Calibrated Existence Gauge",
+    #     'item_id': 168934
+    # },
+    # "20": { # done
+    #     'name': "Life-Binder's Invocation",
+    #     'major': {
+    #         'name': "Life-Binder's Invocation",
+    #         'spell_id': 299944
+    #     },
+    #     'minor':{
+    #         'name': "Seed of Eonar",
+    #         'spell_id': 299940
+    #     },
+    #     'item_name': "Lingering Seed of Renewal",
+    #     'item_id': 168926
+    # },
+    # "21": { # done
+    #     'name': "Vitality Conduit",
+    #     'major': {
+    #         'name': "Vitality Conduit",
+    #         'spell_id': 299959
+    #     },
+    #     'minor':{
+    #         'name': "Transference",
+    #         'spell_id': 303476
+    #     },
+    #     'item_name': "Grid of Bursting Vitality",
+    #     'item_id': 168943
+    # },
+    "22": { # done
+        'name': "Vision of Perfection",
+        'major': {
+            'name': "Vision of Perfection",
+            'spell_id': 299370
+        },
+        'minor':{
+            'name': "Strive for Perfection",
+            'spell_id': 299369
+        },
+        'item_name': "Perfection-Enhancing Gearbox",
+        'item_id': 168844
+    },
+    "23": { # done
+        'name': "Blood of the Enemy",
+        'major': {
+            'name': "Blood of the Enemy",
+            'spell_id': 298277
+        },
+        'minor':{
+            'name': "Blood-Soaked",
+            'spell_id': 298275
+        },
+        'item_name': "Churned Blood of the Conquered",
+        'item_id': 168444
+    },
+    # "25": { # done tank
+    #     'name': "Aegis of the Deep",
+    #     'major': {
+    #         'name': "Aegis of the Deep",
+    #         'spell_id': 299275
+    #     },
+    #     'minor':{
+    #         'name': "Stand Your Ground",
+    #         'spell_id': 299277
+    #     },
+    #     'item_name': "Regenerating Barrier of the Depths",
+    #     'item_id': 168839
+    # },
+    "27": { # done
+        'name': "Memory of Lucid Dreams",
+        'major': {
+            'name': "Memory of Lucid Dreams",
+            'spell_id': 299374
+        },
+        'minor':{
+            'name': "Lucid Dreams",
+            'spell_id': 299373
+        },
+        'item_name': "Pearl of Perspicuous Intentions",
+        'item_id': 168848
+    },
+    "28": { # done
+        'name': "The Unbound Force",
+        'major': {
+            'name': "The Unbound Force",
+            'spell_id': 299378
+        },
+        'minor':{
+            'name': "Reckless Force",
+            'spell_id': 299377
+        },
+        'item_name': "Polarized Azerite Slivershards",
+        'item_id': 168865
+    },
+    "32": { # done
+        'name': "Conflict and Strife",
+        'major': {
+            'name': "Conflict",
+            'spell_id': 304121
+        },
+        'minor':{
+            'name': "Strife",
+            'spell_id': 304123
+        },
+        'item_name': "Rib-Bone Choker of Dominance",
+        'item_id': 169900
+    }
+}
+
 
 def is_melee(wow_class, wow_spec):
     """True if spec is melee spec.
@@ -1964,3 +2240,11 @@ def is_dps_talent_combination(talent_combination, wow_class):
         ][i] == "0":
             return False
     return True
+
+def get_essences() -> dict:
+    """Returns a dictionary of all available dps essences.
+
+    Returns:
+        [type] -- [description]
+    """
+    return __essences
