6994403e8963c3759becdd52aec8cd9c86d51a14
Bloodmallet <kruse.peter.1990@gmail.com>
2019-11-09 20:57:23 +0100

    format file

--- simc_support/simc_checks.py
+++ simc_support/simc_checks.py
@@ -3,111 +3,106 @@
 """
 from typing import Union
 
+
 class FightStyle(object):
-  """Collection of all fight style related information
+    """Collection of all fight style related information
     """
 
-  PATCHWERK = "patchwerk"
-  LIGHTMOVEMENT = "lightmovement"
-  HEAVYMOVEMENT = "heavymovement"
-  HECTICADDCLEAVE = "hecticaddcleave"
-  BEASTLORD = "beastlord"
-  HELTERSKELTER = "helterskelter"
-  DUNGEONSLICE = "dungeonslice"
+    PATCHWERK = "patchwerk"
+    LIGHTMOVEMENT = "lightmovement"
+    HEAVYMOVEMENT = "heavymovement"
+    HECTICADDCLEAVE = "hecticaddcleave"
+    BEASTLORD = "beastlord"
+    HELTERSKELTER = "helterskelter"
+    DUNGEONSLICE = "dungeonslice"
 
+    def get_fight_styles(self) -> tuple:
+        """Returns a touple of all fight styles
 
+        Returns:
+        touple -- Containing all fight styles
+        """
 
-  def get_fight_styles(self) -> tuple:
-    """Returns a touple of all fight styles
+        return (
+            self.PATCHWERK,
+            self.LIGHTMOVEMENT,
+            self.HEAVYMOVEMENT,
+            self.HECTICADDCLEAVE,
+            self.BEASTLORD,
+            self.HELTERSKELTER,
+            self.DUNGEONSLICE,
+        )
 
-    Returns:
-      touple -- Containing all fight styles
-    """
+    def is_fight_style(self, fight_style: Union[str, list]) -> bool:
+        """Returns true if fight_style is known and spelled correctly.
 
-    return (
-      self.PATCHWERK,
-      self.LIGHTMOVEMENT,
-      self.HEAVYMOVEMENT,
-      self.HECTICADDCLEAVE,
-      self.BEASTLORD,
-      self.HELTERSKELTER,
-      self.DUNGEONSLICE
-    )
+        Arguments:
+        fight_style {str,list} -- [description]
 
+        Returns:
+        bool -- Known and spelled correctly fight style/-s
+        """
 
-  def is_fight_style(self, fight_style: Union[str,list]) -> bool:
-    """Returns true if fight_style is known and spelled correctly.
+        # downwards compatibility
+        if type(fight_style) == list:
+            return self.is_fight_style_list(fight_style)
+        fight_style_list = self.get_fight_styles()
+        if not fight_style in fight_style_list:
+            return False
+        return True
 
-    Arguments:
-      fight_style {str,list} -- [description]
+    def is_fight_style_list(self, fight_styles: list) -> bool:
+        """Returns true if all fight styles are spelled correctly and are known.
 
-    Returns:
-      bool -- Known and spelled correctly fight style/-s
-    """
+        Arguments:
+        fight_styles {list} -- [description]
 
-    # downwards compatibility
-    if type(fight_style) == list:
-      return self.is_fight_style_list(fight_style)
-    fight_style_list = self.get_fight_styles()
-    if not fight_style in fight_style_list:
-      return False
-    return True
+        Returns:
+        bool -- [description]
+        """
 
-
-  def is_fight_style_list(self, fight_styles: list) -> bool:
-    """Returns true if all fight styles are spelled correctly and are known.
-
-    Arguments:
-      fight_styles {list} -- [description]
-
-    Returns:
-      bool -- [description]
-    """
-
-    fight_style_list = self.get_fight_styles()
-    for fight_style in fight_styles:
-      if not type(fight_style) == str:
-        return False
-      if not fight_style in fight_style_list:
-        return False
-    return True
-
-
+        fight_style_list = self.get_fight_styles()
+        for fight_style in fight_styles:
+            if not type(fight_style) == str:
+                return False
+            if not fight_style in fight_style_list:
+                return False
+        return True
 
 
 class Tier(object):
-  PR = "PR"
-  T21 = "T21"
-  T22 = "T22"
-  T23 = "T23"
+    PR = "PR"
+    T21 = "T21"
+    T22 = "T22"
+    T23 = "T23"
+    T24 = "T24"
 
+    def get_tiers(self) -> tuple:
+        """Return a tuple of all known tiers.
 
-  def get_tiers(self) -> tuple:
-    """Return a tuple of all known tiers.
+        Returns:
+        tuple -- [description]
+        """
 
-    Returns:
-      tuple -- [description]
-    """
+        return (
+            self.PR,
+            self.T21,
+            self.T22,
+            self.T23,
+            self.T24,
+        )
 
-    return (
-      self.PR,
-      self.T21,
-      self.T22,
-      self.T23
-    )
+    def is_tier(self, tier: str) -> bool:
+        """Returns true if tier is known.
 
+        Arguments:
+        profile {str} -- [description]
 
-  def is_tier(self, tier: str) -> bool:
-    """Returns true if tier is known.
+        Returns:
+        bool -- [description]
+        """
 
-    Arguments:
-      profile {str} -- [description]
-
-    Returns:
-      bool -- [description]
-    """
-
-    for base_profile in self.get_tiers():
-      if tier == base_profile:
-        return True
-    return False
+        for base_profile in self.get_tiers():
+            if tier == base_profile:
+                return True
+        return False
