94a5199ee0fa31d6a9b329f925bd741af3a2687a
Bloodmallet <kruse.peter.1990@gmail.com>
2019-12-29 15:13:34 +0100

    add corruptions data

---
+++ simc_support/game_data/Corruption.py
@@ -0,0 +1,300 @@
+malus = {
+    '1': 10,
+    '2': 15,
+    '3': 20,
+}
+
+corruptions = {
+    'Poison': {
+        '1': {
+            'bonus_id': 6434,
+            'spell_id': 13518
+        }
+    },
+    'Strikethrough': {
+        '1': {
+            'bonus_id': 6437,
+            'spell_id': 315277
+        },
+        '2': {
+            'bonus_id': 6438,
+            'spell_id': 315281
+        },
+        '3': {
+            'bonus_id': 6439,
+            'spell_id': 315282
+        }
+    },
+    'Masterful': {
+        '1': {
+            'bonus_id': 6471,
+            'spell_id': 315529
+        },
+        '2': {
+            'bonus_id': 6472,
+            'spell_id': 315530
+        },
+        '3': {
+            'bonus_id': 6473,
+            'spell_id': 315531
+        }
+    },
+    'Expedient': {
+        '1': {
+            'bonus_id': 6474,
+            'spell_id': 315544
+        },
+        '2': {
+            'bonus_id': 6475,
+            'spell_id': 315545
+        },
+        '3': {
+            'bonus_id': 6476,
+            'spell_id': 315546
+        }
+    },
+    'Versatile': {
+        '1': {
+            'bonus_id': 6477,
+            'spell_id': 315549
+        },
+        '2': {
+            'bonus_id': 6478,
+            'spell_id': 315552
+        },
+        '3': {
+            'bonus_id': 6479,
+            'spell_id': 315553
+        }
+    },
+    'Severe': {
+        '1': {
+            'bonus_id': 6480,
+            'spell_id': 315554
+        },
+        '2': {
+            'bonus_id': 6481,
+            'spell_id': 315557
+        },
+        '3': {
+            'bonus_id': 6482,
+            'spell_id': 315558
+        }
+    },
+    'Avoidant': {
+        '1': {
+            'bonus_id': 6483,
+            'spell_id': 315607
+        },
+        '2': {
+            'bonus_id': 6484,
+            'spell_id': 315608
+        },
+        '3': {
+            'bonus_id': 6485,
+            'spell_id': 315609
+        }
+    },
+    'Glimpse of Clarity': {
+        '1': {
+            'bonus_id': 6486,
+            'spell_id': 315574
+        },
+        '2': {
+            'bonus_id': 6546,
+            'spell_id': 318239
+        }
+    },
+    'Siphoner': {
+        '1': {
+            'bonus_id': 6493,
+            'spell_id': 315590
+        },
+        '2': {
+            'bonus_id': 6494,
+            'spell_id': 315591
+        },
+        '3': {
+            'bonus_id': 6495,
+            'spell_id': 315592
+        }
+    },
+    'Twilight Devastation': {
+        '1': {
+            'bonus_id': 6537,
+            'spell_id': 318276
+        },
+        '2': {
+            'bonus_id': 6538,
+            'spell_id': 318477
+        },
+        '3': {
+            'bonus_id': 6539,
+            'spell_id': 318478
+        }
+    },
+    'Void Ritual': {
+        '1': {
+            'bonus_id': 6540,
+            'spell_id': 318286
+        },
+        '2': {
+            'bonus_id': 6541,
+            'spell_id': 318479
+        },
+        '3': {
+            'bonus_id': 6542,
+            'spell_id': 318480
+        }
+    },
+    'Twisted Appendage': {
+        '1': {
+            'bonus_id': 6543,
+            'spell_id': 318481
+        },
+        '2': {
+            'bonus_id': 6544,
+            'spell_id': 318482
+        },
+        '3': {
+            'bonus_id': 6545,
+            'spell_id': 318483
+        }
+    },
+    'Ineffable Truth': {
+        '1': {
+            'bonus_id': 6547,
+            'spell_id': 318303
+        },
+        '2': {
+            'bonus_id': 6548,
+            'spell_id': 318484
+        }
+    },
+    'Echoing Void': {
+        '1': {
+            'bonus_id': 6549,
+            'spell_id': 318280
+        },
+        '2': {
+            'bonus_id': 6550,
+            'spell_id': 318485
+        },
+        '3': {
+            'bonus_id': 6551,
+            'spell_id': 318486
+        }
+    },
+    'Infinite Stars': {
+        '1': {
+            'bonus_id': 6552,
+            'spell_id': 318274
+        },
+        '2': {
+            'bonus_id': 6553,
+            'spell_id': 318487
+        },
+        '3': {
+            'bonus_id': 6554,
+            'spell_id': 318488
+        }
+    },
+    'Racing Pulse': {
+        '1': {
+            'bonus_id': 6555,
+            'spell_id': 318266
+        },
+        '2': {
+            'bonus_id': 6559,
+            'spell_id': 318492
+        },
+        '3': {
+            'bonus_id': 6560,
+            'spell_id': 318496
+        }
+    },
+    'Deadly Momentum': {
+        '1': {
+            'bonus_id': 6556,
+            'spell_id': 318268
+        },
+        '2': {
+            'bonus_id': 6561,
+            'spell_id': 318493
+        },
+        '3': {
+            'bonus_id': 6562,
+            'spell_id': 318497
+        }
+    },
+    'Honed Mind': {
+        '1': {
+            'bonus_id': 6557,
+            'spell_id': 318269
+        },
+        '2': {
+            'bonus_id': 6563,
+            'spell_id': 318494
+        },
+        '3': {
+            'bonus_id': 6564,
+            'spell_id': 318498
+        }
+    },
+    'Surging Vitality': {
+        '1': {
+            'bonus_id': 6558,
+            'spell_id': 318270
+        },
+        '2': {
+            'bonus_id': 6565,
+            'spell_id': 318495
+        },
+        '3': {
+            'bonus_id': 6566,
+            'spell_id': 318499
+        }
+    },
+    'Devour Vitality': {
+        '1': {
+            'bonus_id': 6567,
+            'spell_id': 318294
+        }
+    },
+    'Whispered Truths': {
+        '1': {
+            'bonus_id': 6568,
+            'spell_id': 316780
+        }
+    },
+    'Lash of the Void': {
+        '1': {
+            'bonus_id': 6569,
+            'spell_id': 317290
+        }
+    },
+    'Flash of Insight': {
+        '1': {
+            'bonus_id': 6570,
+            'spell_id': 318299
+        }
+    },
+    'Searing Flames': {
+        '1': {
+            'bonus_id': 6571,
+            'spell_id': 318293
+        }
+    },
+    'Obsidian Skin': {
+        '1': {
+            'bonus_id': 6572,
+            'spell_id': 316651
+        }
+    },
+    'Gushing Wound': {
+        '1': {
+            'bonus_id': 6573,
+            'spell_id': 318272
+        }
+    }
+}
