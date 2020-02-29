208bc96dd52c77f05d1d9fd8986f65ec89d75e7f
Bloodmallet(EU) <kruse.peter.1990@gmail.com>
2020-03-01 00:35:08 +0100

    update setup of simc_support

--- setup.py
+++ setup.py
@@ -8,17 +8,20 @@ def readme():
         return f.read()
 
 
-setup(name='simc_support',
-      version='8.2.0.0',
-      author='Bloodmallet(EU)',
-      author_email='kruse.peter.1990@gmail.com',
-      description='This package offers some World of Warcraft related ingame data regularly used for simulations and some basic input test for standard values of SimulationCraft.',
-      long_description=readme(),
-      url='https://github.com/Bloodmallet/simc_support',
-      packages=['simc_support'],
-      package_data={
-          "": ["*.md", ],
-      },
-      python_requires='>=3.6',
-      license='GNU GENERAL PUBLIC LICENSE',
-      )
+setup(
+    name='simc_support',
+    version='8.3.0.0',
+    author='Bloodmallet(EU)',
+    author_email='kruse.peter.1990@gmail.com',
+    description='This package offers some World of Warcraft related ingame data regularly used for simulations and some basic input test for standard values of SimulationCraft.',
+    long_description=readme(),
+    url='https://github.com/Bloodmallet/simc_support',
+    packages=[
+        'simc_support',
+    ],
+    package_data={
+        "": ["*.md",],
+    },
+    python_requires='>=3.6',
+    license='GNU GENERAL PUBLIC LICENSE',
+)
