9bd7b83bd73d1828b4e07913c56a501563f91e89
Bloodmallet(EU) <kruse.peter.1990@gmail.com>
2020-07-05 02:13:39 +0200

    replace simc_checks

---
+++ simc_support/simc_data/FightStyle.py
@@ -0,0 +1,17 @@
+BEASTLORD = "beastlord"
+DUNGEONSLICE = "dungeonslice"
+HEAVYMOVEMENT = "heavymovement"
+HECTICADDCLEAVE = "hecticaddcleave"
+HELTERSKELTER = "helterskelter"
+LIGHTMOVEMENT = "lightmovement"
+PATCHWERK = "patchwerk"
+
+FIGHTSTYLES = (
+    BEASTLORD,
+    DUNGEONSLICE,
+    HEAVYMOVEMENT,
+    HECTICADDCLEAVE,
+    HELTERSKELTER,
+    LIGHTMOVEMENT,
+    PATCHWERK,
+)
