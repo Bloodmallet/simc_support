a32e46fd066c6076845dad56c2f216c214ac907f
Bloodmallet(EU) <kruse.peter.1990@gmail.com>
2020-08-31 21:37:53 +0200

    add dbc extraction

--- simc_support/self_update.py
+++ simc_support/self_update.py
@@ -3,11 +3,13 @@ This script updates data_files by using SimulationCrafts casc and dbc extractors
 Basically; execute this script once per patch.
 """
 import argparse
+from genericpath import exists
 import json
 import logging
 import os
 import pathlib
 import subprocess
+import typing
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -18,6 +20,21 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 
 logger.addHandler(ch)
+
+
+_LOCALES = [
+    "en_US",
+    "ko_KR",
+    "fr_FR",
+    "de_DE",
+    "zh_CN",
+    "es_ES",
+    "ru_RU",
+    "it_IT",
+    "pt_PT",
+]
+
+DATA_PATH = "data_files"
 
 
 def handle_arguments() -> argparse.ArgumentParser:
@@ -54,6 +71,11 @@ def handle_arguments() -> argparse.ArgumentParser:
         help="Flag disables download from CDN. Instead only already present local files are used.",
     )
     parser.add_argument(
+        "--no-extract",
+        action="store_true",
+        help="Flag disables exrating from db2 files. Instead only already present local files are used.",
+    )
+    parser.add_argument(
         "--ptr",
         action="store_true",
         help="Download PTR files",
@@ -71,24 +93,41 @@ def handle_arguments() -> argparse.ArgumentParser:
     return parser
 
 
-def is_python() -> bool:
-    output = subprocess.run(["python", "--version"], stdout=subprocess.PIPE)
-    return output.stdout.split()[1].startswith(b"3")
+def get_python() -> str:
+    """Find and return the appropriate commandline input to use Python 3+
 
+    Returns:
+        str: Either "python" or "python3"
+    """
 
-def is_python3() -> bool:
-    output = subprocess.run(["python3", "--version"], stdout=subprocess.PIPE)
-    return output.stdout.split()[1].startswith(b"3")
+    def is_python() -> bool:
+        output = subprocess.run(["python", "--version"], stdout=subprocess.PIPE)
+        return output.stdout.split()[1].startswith(b"3")
 
+    def is_python3() -> bool:
+        output = subprocess.run(["python3", "--version"], stdout=subprocess.PIPE)
+        return output.stdout.split()[1].startswith(b"3")
 
-def casc(args) -> None:
-    if args.no_load:
-        return
-    logger.info("Downloading files (casc)")
     if not (is_python() or is_python3()):
         raise SystemError("Python 3 is required.")
 
-    python = "python" if is_python() else "python3"
+    return "python" if is_python() else "python3"
+
+
+def casc(args: object) -> None:
+    """Download all db2 files of all locales described in _LOCALES.
+
+    Args:
+        args (object): [description]
+
+    Raises:
+        SystemError: If Python 3+ is not available in commandline neither via 'python' nor 'python3'.
+    """
+    if args.no_load:
+        return
+    logger.info("Downloading files (casc)")
+
+    python = get_python()
 
     command = [
         python,
@@ -96,34 +135,104 @@ def casc(args) -> None:
         "--cdn",
         "-m",
         "batch",
-        "-o",
-        args.output,
     ]
     if args.ptr:
         command.append("--ptr")
     elif args.beta:
         command.append("--beta")
 
-    # TODO: Add all locales
+    for locale in _LOCALES:
+        logger.info(locale)
 
-    logger.debug(command)
-    process = subprocess.Popen(
-        command,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        cwd=os.path.join(args.simc, "casc_extract"),
-    )
-    while True:
-        output = process.stdout.readline()
-        if output == b"" and process.poll() is not None:
-            break
-        if output:
-            logger.debug(output.strip().decode("ascii"))
-    rc = process.poll()
-    logger.debug(rc)
+        process = subprocess.Popen(
+            command
+            + [
+                "--locale",
+                locale,
+                "-o",
+                os.path.join(args.output, locale),
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=os.path.join(args.simc, "casc_extract"),
+        )
+        while True:
+            output = process.stdout.readline()
+            if output == b"" and process.poll() is not None:
+                break
+            if output:
+                logger.debug(output.strip().decode("ascii"))
+        rc = process.poll()
+        if rc != 0:
+            logger.error(f"Error occured while loading {locale}.")
 
 
-def update_trinkets(args: dict) -> None:
+def get_compiled_data_path(args: object, locale: str) -> str:
+    return os.path.join(args.output, "compiled", locale)
+
+
+def dbc(args: object, files: typing.List[str]) -> None:
+    """Extract and transforms files into JSON format.
+
+    Args:
+        args (object): [description]
+        files (typing.List[str]): [description]
+    """
+    if args.no_extract:
+        return
+    logger.info("Extracting files (dbc)")
+
+    python = get_python()
+
+    wow_version = os.listdir(os.path.join(args.output, _LOCALES[0]))[0]
+
+    command = [
+        python,
+        "dbc_extract.py",
+        "-b",
+        wow_version,
+        "-t",
+        "json",
+    ]
+    if args.wow:
+        command.append("--hotfix")
+        command.append(os.path.join(args.wow, "Cache/ADB/enUS/DBCache.bin"))
+
+    for file in files:
+        for locale in _LOCALES:
+            logger.info(f"{file} {locale}")
+
+            cmd = command + [
+                "-p",
+                os.path.join(args.output, locale, wow_version, "DBFilesClient"),
+                file,
+            ]
+
+            logger.debug(cmd)
+            pathlib.Path(os.path.join(get_compiled_data_path(args, locale))).mkdir(
+                parents=True, exist_ok=True
+            )
+
+            process = subprocess.run(
+                cmd,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                cwd=os.path.join(args.simc, "dbc_extract3"),
+                universal_newlines=True,
+            )
+            if process.returncode != 0:
+                logger.error(
+                    f"Error occured while extracting {file} {locale}. {process.stderr}"
+                )
+            else:
+                with open(
+                    os.path.join(get_compiled_data_path(args, locale), f"{file}.json"),
+                    "w",
+                ) as f:
+                    f.write(process.stdout)
+
+
+def update_trinkets(args: object) -> None:
 
     DB_FILES = ["ItemSparse"]
 
@@ -152,7 +261,12 @@ def update_trinkets(args: dict) -> None:
     def is_whitelisted(item: dict) -> bool:
         return item.get("id") in WHITELIST
 
-    with open(f"{DB_FILES[0]}.json", "r") as f:
+    dbc(args, DB_FILES)
+
+    with open(
+        os.path.join(get_compiled_data_path(args, _LOCALES[0]), f"{DB_FILES[0]}.json"),
+        "r",
+    ) as f:
         items = json.load(f)
 
     trinkets = [
@@ -166,7 +280,7 @@ def update_trinkets(args: dict) -> None:
 
     logger.debug(f"Count: {len(trinkets)}")
 
-    with open("trinkets.json", "w") as f:
+    with open(os.path.join(DATA_PATH, "trinkets.json"), "w") as f:
         json.dump(trinkets, f)
 
 
@@ -175,7 +289,7 @@ def main() -> None:
     if args.debug:
         logger.setLevel(logging.DEBUG)
     casc(args)
-    # update_trinkets(args)
+    update_trinkets(args)
 
 
 if __name__ == "__main__":
