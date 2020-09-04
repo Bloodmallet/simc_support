459347ad3156354050eb6c9477ba1c715137c41d
Bloodmallet(EU) <kruse.peter.1990@gmail.com>
2020-09-04 23:12:11 +0200

    add an Unknown source

--- simc_support/game_data/Source.py
+++ simc_support/game_data/Source.py
@@ -6,6 +6,7 @@ class Source(enum.Enum):
     PROFESSION = "Profession"
     PVP = "PvP"
     RAID = "Raid"
+    UNKNOWN = "Unknown"
     WORLD_BOSS = "World Boss"
     WORLD_DROP = "World Drop"
     WORLD_QUEST = "World Quest"
