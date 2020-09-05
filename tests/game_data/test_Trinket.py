19659028e6018dc486bc450e0ae478559a89bce2
Bloodmallet(EU) <kruse.peter.1990@gmail.com>
2020-09-05 09:09:06 +0200

    test for availability of each primary stat

--- tests/game_data/test_Trinket.py
+++ tests/game_data/test_Trinket.py
@@ -1,4 +1,5 @@
 from simc_support.game_data.Trinket import TRINKETS
+from simc_support.game_data.Stat import Stat
 import unittest
 
 
@@ -8,3 +9,14 @@ class Trinkets(unittest.TestCase):
 
     def test_non_emptyness(self):
         self.assertTrue(len(TRINKETS) > 0)
+
+    def test_for_each_primary_stat(self):
+        """At least one trinket for each primary stat is available."""
+        for stat in Stat:
+            with self.subTest(stat=stat):
+                found = False
+                for trinket in TRINKETS:
+                    if stat in trinket.stats:
+                        found = True
+                        break
+                self.assertTrue(found)
