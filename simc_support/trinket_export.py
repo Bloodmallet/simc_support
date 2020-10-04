df78a3feb0a5549e51c75147b9108795c2b59d53
Bloodmallet(EU) <kruse.peter.1990@gmail.com>
2020-10-04 18:42:17 +0200

    update trinket exporter for simc

--- simc_support/trinket_export.py
+++ simc_support/trinket_export.py
@@ -1,33 +1,44 @@
 """Exports trinkets from simc_support to three copy= .simc files for testing purpose.
 """
+import typing
 
-import wow_lib
+from simc_support.game_data.WowSpec import get_wow_spec
+from simc_support.game_data.Trinket import get_trinkets_for_spec, Trinket
 
 
-def print_simc_file(trinket_list, name):
-    with open("trinkets_bfa_" + name + ".simc", 'w') as f:
+def print_simc_file(trinket_list: typing.List[Trinket], name: str):
+    with open("trinkets_shadowlands_" + name + ".simc", "w") as f:
         f.write("# PROFILE FOR TESTING ONLY!\n")
-        f.write("# This file provides all available trinkets for {}s.\n".format(name.upper()))
-        f.write("# Use this file to verify whether all trinkets are functioning as expected after changes.\n")
+        f.write(
+            "# This file provides all available trinkets for {}s.\n".format(
+                name.upper()
+            )
+        )
+        f.write(
+            "# Use this file to verify whether all trinkets are functioning as expected after changes.\n"
+        )
         f.write("# No appropriate drop itemlevel required.\n\n\n")
 
         for trinket in trinket_list:
-            t_name, t_id, _, t_ilevel, _, _ = trinket
-            f.write("copy=\"{}\"\n".format(t_name))
-            f.write("trinket1=,id={},ilevel={}\n\n".format(t_id, t_ilevel))
+            f.write('copy="{}"\n'.format(trinket.name))
+            f.write(
+                "trinket1=,id={},ilevel={}\n\n".format(
+                    trinket.item_id, trinket.min_itemlevel
+                )
+            )
 
 
 def main():
 
-    trinkets = wow_lib.get_trinkets_for_spec("warrior", "arms")
+    trinkets = get_trinkets_for_spec(get_wow_spec("warrior", "arms"))
     print_simc_file(trinkets, "melee")
 
-    trinkets = wow_lib.get_trinkets_for_spec("hunter", "marksmanship")
+    trinkets = get_trinkets_for_spec(get_wow_spec("hunter", "marksmanship"))
     print_simc_file(trinkets, "hunter")
 
-    trinkets = wow_lib.get_trinkets_for_spec("shaman", "elemental")
+    trinkets = get_trinkets_for_spec(get_wow_spec("shaman", "elemental"))
     print_simc_file(trinkets, "caster")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
