d74876de07fcfa84ab0a1a3d2a55ed9823be1675
Bloodmallet(EU) <kruse.peter.1990@gmail.com>
2020-10-25 21:38:10 +0100

    add more output to publish-script

--- publish_release.sh
+++ publish_release.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
-. env/bin/activate
+echo "Activating environment"
+. ./env/bin/activate
 
+echo "Removing old artifacts"
 for D in "build" "dist" "simc_support.egg-info"; do
     if [ -d "${D}" ]; then
         rm -r ${D}
     fi
 done
 
+echo "Install requirements"
 pip install -U -r requirements.txt
 pip install -U setuptools wheel twine
 
+echo "Execute unittest"
 python -m unittest
 
+echo "Build wheel"
 python setup.py sdist bdist_wheel
+
+echo "Publish"
 python -m twine upload dist/*
