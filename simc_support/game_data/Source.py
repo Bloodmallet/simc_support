4eeac4c2d5305f2d51936ceeb715831d84ce92db
Bloodmallet <bloodmalleteu@gmail.com>
2020-12-07 21:13:01 +0000

    add Calling and Mission Source

--- simc_support/game_data/Source.py
+++ simc_support/game_data/Source.py
@@ -2,12 +2,14 @@ import enum
 
 
 class Source(enum.Enum):
+    CALLING = "Calling"
     DUNGEON = "Dungeon"
+    MISSION = "Mission"
     PROFESSION = "Profession"
     PVP = "PvP"
     RAID = "Raid"
-    UNKNOWN = "Unknown"
     RARE_MOB = "Rare Mob"
+    UNKNOWN = "Unknown"
     WORLD_BOSS = "World Boss"
     WORLD_DROP = "World Drop"
     WORLD_QUEST = "World Quest"
