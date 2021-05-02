06ed402d175167f4f624dcb140898466dca4c684
Bloodmallet <bloodmalleteu@gmail.com>
2021-05-02 16:51:41 +0200

    fix type hint

--- simc_support/game_data/Talent.py
+++ simc_support/game_data/Talent.py
@@ -101,7 +101,7 @@ def get_talent_dict(wow_spec: WowSpec, ptr: bool = None) -> dict:
     return result
 
 
-def get_talents_for_spec(wow_spec: WowSpec) -> typing.Tuple[Talent]:
+def get_talents_for_spec(wow_spec: WowSpec) -> typing.Tuple[Talent, ...]:
     return tuple(
         [
             talent
