1894d2abe542a17bf551fd6ecd8fc462e5c77709
Bloodmallet <bloodmalleteu@gmail.com>
2021-05-28 19:34:08 +0200

    make versioning tag dependent

--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ def readme():
 
 setup(
     name="simc_support",
-    # schema: <wow_version, e.g. 9.0.1>.<own_version_per_wow_version><a/b/rc alpha/beta/release candidate bit><bit_id>
-    version="9.0.5.2",
     author="Bloodmallet(EU)",
     author_email="bloodmalleteu@gmail.com",
     description="Data to support simulations for World of Warcraft with SimulationCraft.",
@@ -32,4 +30,16 @@ setup(
     },
     python_requires=">=3.6",
     license="GNU GENERAL PUBLIC LICENSE",
+    version_config={
+        "template": "{tag}",
+        "dev_template": "{tag}+{branch}.{ccount}.{sha}",
+        "dirty_template": "{tag}+{branch}.{ccount}.{sha}.dirty",
+        # schema: <wow_version, e.g. 9.0.1>.<own_version_per_wow_version>
+        "starting_version": "0.0.0.1",
+        "version_callback": None,
+        "version_file": None,
+        "count_commits_from_version_file": False,
+        "branch_formatter": None,
+    },
+    setup_requires=["setuptools-git-versioning"],
 )
