ff859b0235b13fe8dfc8d891af58e974eb29414a
Bloodmallet <bloodmalleteu@gmail.com>
2021-06-27 08:57:02 +0200

    fix version-labeling scheme

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ setup(
     license="GNU GENERAL PUBLIC LICENSE",
     version_config={
         "template": "{tag}",
-        "dev_template": "{tag}+{branch}.{ccount}.{sha}",
-        "dirty_template": "{tag}+{branch}.{ccount}.{sha}.dirty",
+        "dev_template": "{tag}.dev1+{branch}.{ccount}.{sha}",
+        "dirty_template": "{tag}.dev1+{branch}.{ccount}.{sha}.dirty",
         # schema: <wow_version, e.g. 9.0.1>.<own_version_per_wow_version>
         "starting_version": "0.0.0.1",
         "version_callback": None,
