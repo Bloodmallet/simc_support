127556a3d34bcb54695809018fddca7be2a2068b
Bloodmallet <bloodmalleteu@gmail.com>
2022-05-07 13:49:48 +0200

    temporarily ignore choice nodes and unpack them later

--- simc_support/game_data/new_talent.py
+++ simc_support/game_data/new_talent.py
@@ -1,7 +1,8 @@
 import datetime
-import typing
-import logging
 import enum
+import itertools
+import logging
+import typing
 
 logger = logging.getLogger(__name__)
 T = typing.TypeVar("T")
@@ -51,6 +52,7 @@ class Talent:
         parent_names: typing.Tuple[str, ...] = tuple(),
         children_names: typing.Tuple[str, ...] = tuple(),
         sibling_names: typing.Tuple[str, ...] = tuple(),
+        **kwargs,
     ) -> None:
         self.name: str = name
         self.talent_type: TalentType = talent_type
@@ -65,6 +67,7 @@ class Talent:
         self.siblings: typing.Tuple["Talent", ...] = tuple()
 
     def __repr__(self) -> str:
+        # return f"{self.name}({self.index})"
         return f"{self.name}:{self.index}(p:{self.parent_names},c:{self.children_names},s:{self.sibling_names})"
 
     @property
@@ -110,19 +113,6 @@ class Talent:
         if self.is_selected(tree):
             raise AlreadySelectedError(
                 f"Node {self.name} at index {self.index} can't be selected at {tree} because node was already selected."
-            )
-
-        # because trees are grown this is not required
-        # if len(self.parents) > 0 and not any(
-        #     [parent.is_selected(path) for parent in self.parents]
-        # ):
-        #     raise MissingSelectedParentError(
-        #         f"Node {self.name} at index {self.index} can't be selected at {path} because no parent was selected."
-        #     )
-
-        if self.has_selected_siblings(tree):
-            raise SiblingAlreadySelectedError(
-                f"Node {self.name} at index {self.index} can't be selected at {tree} because one of its siblings was already selected."
             )
 
         if not self.is_gate_satisfied(tree):
@@ -227,6 +217,9 @@ def _talent_post_init(talents: typing.Tuple[Talent, ...]) -> typing.Tuple[Talent
 
     for t in talents:
         if not t.is_initialized:
+            logger.error(
+                f"{t.index}>-1, {len(t.parent_names)}=={len(t.parents)}, {len(t.children_names)}=={len(t.children)}"
+            )
             raise InitializationError(t)
 
     return talents
@@ -604,6 +597,304 @@ def _create_talents() -> typing.Tuple[Talent, ...]:
     return tuple(talents.talents)
 
 
+def _create_no_choice_talents() -> typing.Tuple[Talent, ...]:
+    """See /simc_support/game_data/partial_tree.jpg
+
+    Returns:
+        typing.List[TreeNode]: Representation of /simc_support/game_data/partial_tree.jpg
+    """
+
+    class HelperTalents:
+        def __init__(self) -> None:
+            self.talents: typing.List[Talent] = []
+
+        def append(self, talent: typing.Union[Talent, typing.Iterable[Talent]]) -> None:
+            if isinstance(talent, Talent):
+                self.talents.append(talent)
+            elif isinstance(talent, typing.Iterable):
+                for t in talent:
+                    self.talents.append(t)
+            else:
+                pass
+
+    talents = HelperTalents()
+
+    # row 1
+    talents.append(
+        Talent(
+            name="A1",
+            talent_type=TalentType.ABILITY,
+        )
+    )
+    # row 2
+    talents.append(
+        Talent(
+            name="B1",
+            talent_type=TalentType.ABILITY,
+            parent_names=("A1",),
+        ),
+    )
+    talents.append(
+        Talent.create_ranks(
+            name="B2",
+            talent_type=TalentType.PASSIVE,
+            max_rank=2,
+            parent_names=("A1",),
+        ),
+    )
+    talents.append(
+        Talent(
+            name="B3",
+            talent_type=TalentType.PASSIVE,
+            parent_names=("A1",),
+        )
+    )
+    # row 3
+    talents.append(
+        Talent(
+            name="C1",
+            talent_type=TalentType.ABILITY,
+            parent_names=("B1",),
+        )
+    )
+    talents.append(
+        Talent(
+            name="C2",
+            talent_type=TalentType.ABILITY,
+            parent_names=("B2",),
+        )
+    )
+    talents.append(
+        Talent(
+            name="C3",
+            talent_type=TalentType.ABILITY,
+            parent_names=("B3",),
+        )
+    )
+    # row 4
+    talents.append(
+        Talent.create_ranks(
+            name="D1",
+            talent_type=TalentType.PASSIVE,
+            max_rank=2,
+            parent_names=("C1",),
+        )
+    )
+    talents.append(
+        Talent.create_ranks(
+            name="D2",
+            talent_type=TalentType.PASSIVE,
+            max_rank=2,
+            parent_names=("C3",),
+        )
+    )
+    talents.append(
+        Talent.create_ranks(
+            name="D3",
+            talent_type=TalentType.PASSIVE,
+            max_rank=2,
+            parent_names=("C3",),
+        )
+    )
+    # row 5
+    talents.append(
+        Talent.create_ranks(
+            name="E1",
+            talent_type=TalentType.PASSIVE,
+            required_invested_points=6,
+            max_rank=3,
+            parent_names=("C1",),
+        )
+    )
+    talents.append(
+        Talent(
+            name="E2",
+            talent_type=TalentType.CHOICE,
+            required_invested_points=6,
+            parent_names=(
+                "D1",
+                "D2",
+            ),
+            sibling_names=("A1",),
+        )
+    )
+    talents.append(
+        Talent(
+            name="E4",
+            talent_type=TalentType.PASSIVE,
+            required_invested_points=6,
+            parent_names=("C3",),
+        )
+    )
+    # row 6
+    talents.append(
+        Talent(
+            name="F1",
+            talent_type=TalentType.PASSIVE,
+            parent_names=("E1",),
+        )
+    )
+    talents.append(
+        Talent.create_ranks(
+            name="F2",
+            talent_type=TalentType.PASSIVE,
+            max_rank=2,
+            parent_names=("E2",),
+        )
+    )
+    talents.append(
+        Talent.create_ranks(
+            name="F3",
+            talent_type=TalentType.PASSIVE,
+            max_rank=2,
+            parent_names=("E2",),
+        )
+    )
+    talents.append(
+        Talent(
+            name="F4",
+            talent_type=TalentType.PASSIVE,
+            parent_names=("E4",),
+        )
+    )
+
+    # row 7
+    talents.append(
+        Talent(
+            name="G1",
+            talent_type=TalentType.CHOICE,
+            parent_names=(
+                "F1",
+                "F2",
+            ),
+            sibling_names=("A1",),
+        )
+    )
+    talents.append(
+        Talent(
+            name="G3",
+            talent_type=TalentType.PASSIVE,
+            parent_names=(
+                "F3",
+                "F4",
+            ),
+        )
+    )
+    talents.append(
+        Talent.create_ranks(
+            name="G4",
+            talent_type=TalentType.PASSIVE,
+            max_rank=2,
+            parent_names=("F4",),
+        )
+    )
+
+    # row 8
+    talents.append(
+        Talent(
+            name="H1",
+            talent_type=TalentType.CHOICE,
+            required_invested_points=12,
+            parent_names=("F1",),
+        )
+    )
+    talents.append(
+        Talent(
+            name="H3",
+            talent_type=TalentType.PASSIVE,
+            required_invested_points=12,
+            parent_names=(
+                "G1",
+                "G3",
+            ),
+        )
+    )
+    talents.append(
+        Talent(
+            name="H4",
+            talent_type=TalentType.ABILITY,
+            required_invested_points=12,
+            parent_names=("G4",),
+        )
+    )
+
+    # row 9
+    talents.append(
+        Talent(
+            name="I1",
+            talent_type=TalentType.PASSIVE,
+            parent_names=("H1",),
+        )
+    )
+    talents.append(
+        Talent(
+            name="I2",
+            talent_type=TalentType.PASSIVE,
+            parent_names=("H1",),
+        )
+    )
+    talents.append(
+        Talent.create_ranks(
+            name="I3",
+            talent_type=TalentType.PASSIVE,
+            max_rank=2,
+            parent_names=(
+                "H1",
+                "H3",
+            ),
+        )
+    )
+    talents.append(
+        Talent.create_ranks(
+            name="I4",
+            talent_type=TalentType.PASSIVE,
+            max_rank=2,
+            parent_names=(
+                "H3",
+                "H4",
+            ),
+        )
+    )
+    talents.append(
+        Talent(
+            name="I5",
+            talent_type=TalentType.PASSIVE,
+            parent_names=("H4",),
+        )
+    )
+
+    # row 10
+    talents.append(
+        Talent(
+            name="J1",
+            talent_type=TalentType.CHOICE,
+            parent_names=("I1",),
+            sibling_names=("A1",),
+        )
+    )
+    talents.append(
+        Talent(
+            name="J3",
+            talent_type=TalentType.CHOICE,
+            parent_names=(
+                "I3",
+                "I4",
+            ),
+            sibling_names=("A1",),
+        )
+    )
+    talents.append(
+        Talent(
+            name="J5",
+            talent_type=TalentType.CHOICE,
+            parent_names=("I5",),
+            sibling_names=("A1",),
+        )
+    )
+
+    return tuple(talents.talents)
+
+
 TALENTS: typing.Tuple[Talent, ...] = _talent_post_init(_create_talents())
 
 
@@ -645,14 +936,83 @@ def grow(
     return tuple(existing_paths)
 
 
+def remove_choices(talents: typing.Tuple[Talent, ...]) -> typing.Tuple[Talent, ...]:
+    """Removes sibling/choice nodes by ensuring only one represents them. New talents are created."""
+    single_choiced: typing.List[Talent] = []
+    ignored_nodes: typing.List[str] = []
+    for t in talents:
+        if all([t.name < s for s in t.sibling_names]):
+            single_choiced.append(Talent(**t.__dict__))
+        else:
+            logger.info(f"Temporarily ignoring {t.name}.")
+            ignored_nodes.append(t.name)
+    # remove ignored nodes from parent names and children
+    for t in single_choiced:
+        t.sibling_names = ()
+        t.siblings = ()
+        t.parents = ()
+        t.children = ()
+        t.parent_names = tuple((p for p in t.parent_names if p not in ignored_nodes))
+        t.children_names = ()
+
+    return _talent_post_init(tuple(single_choiced))
+
+
+def readd_choices(
+    talents: typing.Tuple[Talent, ...],
+    single_choice_talents: typing.Tuple[Talent, ...],
+    paths: typing.List[typing.Tuple[bool, ...]],
+) -> typing.Tuple[typing.Tuple[bool, ...]]:
+    no_choice_to_talents_map: typing.Dict[Talent, Talent] = {}
+    for n in single_choice_talents:
+        for t in talents:
+            if n.name == t.name:
+                no_choice_to_talents_map[n] = t
+
+    _original_choices = {
+        n: tuple([n] + list(n.siblings)) for n in talents if n.sibling_names
+    }
+
+    prepared_choices: typing.Dict[Talent, typing.Tuple[Talent]] = {}
+    for n in single_choice_talents:
+        for c in _original_choices:
+            if n.name == c.name:
+                prepared_choices[n] = _original_choices[c]
+
+    trees: typing.List[typing.Tuple[bool, ...]] = []
+    for path in paths:
+        included_choice_nodes = {
+            n: v for n, v in prepared_choices.items() if n.is_selected(path)
+        }
+        blueprint = [False for _ in talents]
+        for talent in single_choice_talents:
+            if talent not in included_choice_nodes and talent.is_selected(path):
+                blueprint[no_choice_to_talents_map[talent].index] = True
+        blueprint = tuple(blueprint)
+
+        choice_combinations = itertools.product(*included_choice_nodes.values())
+        for combination in choice_combinations:
+            talent: Talent
+            local_copy = [v for v in blueprint]
+            for talent in combination:
+                local_copy = list(talent.select(tuple(local_copy)))
+            trees.append(tuple(local_copy))
+
+    return tuple(trees)
+
+
 def igrow(
     talents: typing.Tuple[Talent, ...], points: int
 ) -> typing.Tuple[typing.Tuple[bool, ...], ...]:
 
-    empty_path = [False for _ in talents]
+    prepared_talents = remove_choices(talents)
+
+    empty_path = [False for _ in prepared_talents]
 
     existing_paths: typing.Dict[typing.Tuple[bool, ...], typing.Set[Talent]] = {}
-    existing_paths[tuple(empty_path)] = {t for t in talents if len(t.parents) == 0}
+    existing_paths[tuple(empty_path)] = {
+        t for t in prepared_talents if len(t.parents) == 0
+    }
 
     for invested_points in range(points + 1):
         start_time = datetime.datetime.utcnow()
@@ -689,4 +1049,10 @@ def igrow(
         logger.info(
             f"{invested_points}: {len(existing_paths)} ({datetime.datetime.utcnow()-start_time})"
         )
-    return tuple(existing_paths)
+
+    logger.info("Unpacking choice nodes")
+    start_time = datetime.datetime.utcnow()
+    trees = readd_choices(talents, prepared_talents, list(existing_paths.keys()))
+    logger.info(f"Unpacked: {len(trees)} ({datetime.datetime.utcnow()-start_time})")
+
+    return tuple(trees)
